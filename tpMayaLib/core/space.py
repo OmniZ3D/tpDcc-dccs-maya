666f52df49f0aef3ff1c97bd06a812c35d4f4ee7
Tomi Poveda <tpovedatd@gmail.com>
2020-02-08 02:55:03 +0100

    Added functions to handle Maya Sequencer

--- tpMayaLib/core/space.py
+++ tpMayaLib/core/space.py
@@ -7,10 +7,14 @@ Module that contains functions and classes related with spaces
 
 from __future__ import print_function, division, absolute_import
 
+import logging
+
 from tpPyUtils import python
 
 import tpMayaLib as maya
-from tpMayaLib.core import name as name_utils, attribute as attr_utils, transform as xform_utils
+from tpMayaLib.core import name as name_utils, attribute as attr_utils, transform as xform_utils, constraint as cns_utils
+
+LOGGER = logging.getLogger()
 
 
 def create_follow_group(source_transform, target_transform, prefix='follow', follow_scale=False, use_duplicate=False):
@@ -77,7 +81,114 @@ def create_local_follow_group(source_transform, target_transform, prefix='follow
     if connect_scale:
         attr_utils.connect_scale(source_transform, follow_group)
 
+    maya.cmds.parent(target_transform, follow_group)
+
     if parent:
         maya.cmds.parent(xform_grp, parent)
+
+    return follow_group
+
+
+def create_multi_follow_direct(source_list, target_transform, node, constraint_type='parentConstraint', attribute_name='follow', value=None):
+    """
+    Creates a group above the target that is constrained to multiple transforms. A switch attribute switches their
+    state on/off. Constraints will be "directly" added on the target transform
+    :param source_list: list(str), list of transforms that the target should be constrained by
+    :param target_transform: str, name of a transform node that should follow the transforms in source_list
+    :param node: str, name of the node to add the swtich attribute to
+    :param constraint_type: str, Maya constraint type ('parentConstraint', 'pointConstraint' or 'orientConstraint')
+    :param attribute_name: str, name of the swtich attribute t oadd to the the node
+    :param value: float, value to give the switch attribute on the node
+    :return: str, name of the new group
+    """
+
+    locators = list()
+
+    if attribute_name == 'follow':
+        var = attr_utils.EnumAttribute('FOLLOW')
+        var.create(node)
+
+    for source in source_list:
+        locator = maya.cmds.spaceLocator(n=name_utils.find_unique_name('follower_1_{}'.format(source), False))[0]
+        maya.cmds.hide(locator)
+        match = xform_utils.MatchTransform(target_transform, locator)
+        match.translation_rotation()
+        maya.cmds.parent(locator, source)
+        locators.append(locator)
+
+    if constraint_type == 'parentConstraint':
+        constraint = maya.cmds.parentConstraint(locators, target_transform, mo=True)[0]
+        maya.cmds.setAttr('{}.interpType'.format(constraint), 2)
+    elif constraint_type == 'pointConstraint':
+        constraint = maya.cmds.pointConstraint(locators, target_transform, mo=True)[0]
+    elif constraint_type == 'orientConstraint':
+        constraint = maya.cmds.orientConstraint(locators, target_transform, mo=True)[0]
+        maya.cmds.setAttr('{}.interpType'.format(constraint), 2)
+    else:
+        raise RuntimeError('Constraint Type: "{}" is not supported!'.format(constraint_type))
+
+    constraint_editor = cns_utils.Constraint()
+    constraint_editor.create_switch(node, attribute_name, constraint)
+
+    if value is None:
+        value = (len(source_list) - 1)
+    maya.cmds.setAttr('{}.{}'.format(node, attribute_name), value)
+
+    return target_transform
+
+
+def create_multi_follow(source_list, target_transform, node=None, constraint_type='parentConstraint', attribute_name='follow', value=None, create_title=True):
+    """
+    Creates a group above the target that is constrained to multiple transforms. A switch attribute switches their
+    state on/off. Constraints will be "directly" added on the target transform
+    :param source_list: list(str), list of transforms that the target should be constrained by
+    :param target_transform: str, name of a transform node that should follow the transforms in source_list
+    :param node: str, name of the node to add the swtich attribute to
+    :param constraint_type: str, Maya constraint type ('parentConstraint', 'pointConstraint' or 'orientConstraint')
+    :param attribute_name: str, name of the swtich attribute t oadd to the the node
+    :param value: float, value to give the switch attribute on the node
+    :param create_title: bool
+    :return: str, name of the new group
+    """
+
+    if len(source_list) < 2:
+        LOGGER.warning('Cannot create mlti follow with less than 2 source transforms!')
+        return False
+
+    locators = list()
+
+    if node is None:
+        node = target_transform
+
+    follow_group = xform_utils.create_buffer_group(target_transform, 'follow')
+    title_name = attribute_name.upper()
+
+    for source in source_list:
+        locator = maya.cmds.spaceLocator(n=name_utils.find_unique_name('follower_1_{}'.format(source), False))[0]
+        maya.cmds.hide(locator)
+        match = xform_utils.MatchTransform(target_transform, locator)
+        match.translation_rotation()
+        maya.cmds.parent(locator, source)
+        locators.append(locator)
+
+    if constraint_type == 'parentConstraint':
+        constraint = maya.cmds.parentConstraint(locators, follow_group, mo=True)[0]
+        maya.cmds.setAttr('{}.interpType'.format(constraint), 2)
+    elif constraint_type == 'pointConstraint':
+        constraint = maya.cmds.pointConstraint(locators, follow_group, mo=True)[0]
+    elif constraint_type == 'orientConstraint':
+        constraint = maya.cmds.orientConstraint(locators, follow_group, mo=True)[0]
+        maya.cmds.setAttr('{}.interpType'.format(constraint), 2)
+    else:
+        raise RuntimeError('Constraint Type: "{}" is not supported!'.format(constraint_type))
+
+    constraint_editor = cns_utils.Constraint()
+    if create_title:
+        constraint_editor.create_title(node, constraint, title_name)
+    constraint_editor.create_switch(node, attribute_name, constraint)
+
+    if value is None:
+        value = (len(source_list) - 1)
+    maya.cmds.setAttr('{}.{}'.format(node, attribute_name), value)
 
     return follow_group
